c6d11004350ff509d97bf932d119fb11b30d8a68
WhiteDh <100050383+WhiteDh@users.noreply.github.com>
2023-01-06 00:26:02 +0200

    simple bot for check

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import telebot
-
-bot = telebot.TeleBot("TOKEN", parse_mode="HTML")
+from config import *
+from telebot import types
+bot = telebot.TeleBot(TELEGRAM_BOT_API, parse_mode="HTML")
 
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+def phone (message):
+    print(message.from_user.first_name)
+    print(message)
+    keyboard = types.ReplyKeyboardMarkup (row_width = 1, resize_keyboard = True)
+    button_phone = types.KeyboardButton (text ="send number",request_contact = True)
+    keyboard.add (button_phone)
+    bot.send_message (message.chat.id, 'Phone number', reply_markup = keyboard)
 
-
-@bot.message_handler(func=lambda m: True)
+@bot.message_handler()
 def echo_all(message):
     bot.reply_to(message, message.text)
 
