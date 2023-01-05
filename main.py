ddeb4a6284c1f26b1506a345fa2eb5074eee59a9
WhiteDh <100050383+WhiteDh@users.noreply.github.com>
2023-01-06 00:03:47 +0200

    simple bot for check

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("f")
+import telebot
+
+bot = telebot.TeleBot("TOKEN", parse_mode="HTML")
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
