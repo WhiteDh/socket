669485bc1d5dba8044934a2f912b59fc3dfdc369
Zebaro24-Stud <zebaro24@stu.cn.ua>
2023-02-05 23:37:49 +0200

    server.py

--- server.py
+++ server.py
@@ -2,19 +2,57 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import time
+from threading import Thread
+
 
 sock = socket.socket()
 sock.bind(('', 25565))
 sock.listen(1)
 conn, addr = sock.accept()
-
 print('connected:', addr)
+sock.listen(1)
+conn2, addr2 = sock.accept()
+print('connected:', addr2)
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
-    print(data)
+def get():
+    while True:
+        data = conn.recv(1024)
+        if data:
+            conn2.send(data)
+            print(data.decode("utf-8"))
+
+def get2():
+    while True:
+        data = conn2.recv(1024)
+        if data:
+            conn.send(data)
+            print(data.decode("utf-8"))
+
+
+
+
+Thread(target=get).start()
+get2()
+
 
 conn.close()
+
+
+# import socket
+#
+# sock = socket.socket()
+# sock.bind(('', 25565))
+# sock.listen(1)
+# conn, addr = sock.accept()
+#
+# print('connected:', addr)
+#
+# while True:
+#     data = conn.recv(1024)
+#     print(data)
+#     if not data:
+#         break
+#     conn.send(data.upper())
+#
+# conn.close()
